7f439195b9133ca29cb7f560fb63127e21bd697f
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-09 19:40:33 +0530

    makening the UI

--- store/src/components/Side.jsx
+++ store/src/components/Side.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const Side = () => {
   return (
     <div className='h-20 flex justify-between items-center  w-full bg-blue-400'>
-        <h2>Devashish Graphics</h2>
-        <div>
+        <h2 className=' text-2xl font-semibold'>Devashish Graphics</h2>
+        <div className='flex gap-10 text-2xl'>
           <a href="">Home</a>
           <a href="">About</a>
-          <a href=""></a>
+          <a href="">Store</a>
+          <a href="">Products</a>
         </div>
     </div>
   )
