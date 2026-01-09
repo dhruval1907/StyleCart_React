1781a07ac67d4bb8c0aa842f167fae67807db9a6
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-09 19:50:56 +0530

    making the UI

--- store/src/App.jsx
+++ store/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Side from './components/Side'
 import Main from './components/Main'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Products from './components/Products'
+import Store from './components/Store'
 const App = () => {
   return (
     <div className=''>
         <Main/>
+        <Routes>
+          <Route path='/' element={<Home/> }/>
+          <Route path='/about' element={<About/> }/>
+          <Route path='/product' element={<Products/> }/>
+          <Route path='/store' element={<Store/> }/>
+        </Routes>
     </div>
   )
 }
--- store/src/components/Side.jsx
+++ store/src/components/Side.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Side = () => {
   return (
     <div className='h-20 flex justify-between items-center  w-full bg-blue-400'>
         <h2 className=' text-2xl font-semibold'>Devashish Graphics</h2>
         <div className='flex gap-10 text-2xl'>
-          <a href="">Home</a>
-          <a href="">About</a>
-          <a href="">Store</a>
-          <a href="">Products</a>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
+          <Link to="/product">Products</Link>
         </div>
     </div>
   )
