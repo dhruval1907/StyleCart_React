4c2cbeb8a39fc3724f870485527647c1033b5c05
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-11 19:17:17 +0530

    importing the scss

---
+++ astratto/Astratto/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "../components/Nav.module.scss";
+const Nav = () => {
+  return (
+    <div className={styles.nav}>Nav</div>
+  )
+}
+
+export default Nav
