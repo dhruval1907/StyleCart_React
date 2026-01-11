56bb8a0e4b37105fc8e63681db6cb82e578b67e9
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-11 19:36:14 +0530

    styling the stuff

--- astratto/Astratto/src/components/Nav.jsx
+++ astratto/Astratto/src/components/Nav.jsx
@@ -3,7 +3,9 @@ import "../styles/nav.scss"
 const Nav = () => {
   return (
     <div className="nav">
-      <div className='nav2'></div>
+      <div className='nav2'>
+        <div></div>
+      </div>
     </div>
   )
 }
