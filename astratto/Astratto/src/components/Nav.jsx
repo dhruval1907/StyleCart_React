d8ba3e2db91fee0f92243ddf53382ee9ce8c14e0
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-11 19:51:17 +0530

    styling stuff

--- astratto/Astratto/src/components/Nav.jsx
+++ astratto/Astratto/src/components/Nav.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import "../styles/nav.scss"
+import React from "react";
+import "../styles/nav.scss";
+import { FaStarOfLife } from "react-icons/fa6";
+import { MdOutlineArrowOutward } from "react-icons/md";
+
 const Nav = () => {
   return (
     <div className="nav">
-      <div className='nav2'>
-        <div></div>
+      <div className="nav2">
+        <div className="nav-part1">
+          <div className="icon-main">
+            <FaStarOfLife className="icon" />
+          </div>
+          <h1>Astratto</h1>
+        </div>
+        <div className="nav-part2">
+          <h4>Careers</h4>
+          <a href="">
+            Contact us 
+          <MdOutlineArrowOutward className="icon"/>
+          </a>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
