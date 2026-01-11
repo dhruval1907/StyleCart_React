2a7d958a50084cc0ed870c34f5f93b6b39803bd4
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-11 20:51:44 +0530

    styling the stuff

--- astratto/Astratto/src/components/Main.jsx
+++ astratto/Astratto/src/components/Main.jsx
@@ -4,7 +4,7 @@ import Nav from './Nav'
 
 const Main = () => {
   return (
-    <div>
+    <div >
       <Nav/>
       <Mainpart1/>
     </div>
--- astratto/Astratto/src/components/Mainpart1.jsx
+++ astratto/Astratto/src/components/Mainpart1.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import "../styles/mainpart1.scss"
 const Mainpart1 = () => {
   return (
-    <div>Mainpart1</div>
+    <div className='mainpart1'>
+        <div className='main1-part1'>
+          asdfv 
+        </div>
+    </div>
   )
 }
 
