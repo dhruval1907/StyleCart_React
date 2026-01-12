08c6ed8510f240f0cdd68e2df31652fef17703dd
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-12 20:15:38 +0530

    styliunig the stuff

--- astratto/Astratto/src/components/Mainpart1.jsx
+++ astratto/Astratto/src/components/Mainpart1.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import "../styles/mainpart1.scss"
+import React from "react";
+import "../styles/mainpart1.scss";
+import video from "../assets/hero.mp4";
 const Mainpart1 = () => {
   return (
-    <div className='mainpart1'>
-        <div className='main1-part1'>
-          asdfv 
+    <div className="mainpart1">
+      <div className="inside-main">
+        <div className="inside-part1">
+          <video src={video} loop autoPlay muted></video>
         </div>
-    </div>
-  )
-}
+        <div className="inside-part2">
 
-export default Mainpart1
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Mainpart1;
