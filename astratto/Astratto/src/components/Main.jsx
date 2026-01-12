d3271fcf365164ded66886c441f88d627d942d6d
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-12 22:17:10 +0530

    enough for the day

--- astratto/Astratto/src/components/Main.jsx
+++ astratto/Astratto/src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Mainpart1 from './Mainpart1'
 import Nav from './Nav'
+import Mainpart2 from './Mainpart2'
 
 const Main = () => {
   return (
     <div >
       <Nav/>
       <Mainpart1/>
+      <Mainpart2/>
     </div>
   )
 }
---
+++ astratto/Astratto/src/components/Mainpart2.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/mainpart2.scss";
+const Mainpart2 = () => {
+  return (
+    <div className="mainpart2">
+      <div className="mainpart">
+        <div className="mainpart1">
+          <h1>Our Services</h1>
+          <p>
+            Discover a wide range of opportunities thorugh a comprehensive range
+            of qualified services
+          </p>
+        </div>
+        <div className="mainpart2"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Mainpart2;
